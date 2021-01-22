625221ac362faa69f5e4475676a52697f4bf43e7
Derviş Kopuz <derviskopuz@gmail.com>
2021-01-22 23:51:37 +0300

    init glob vars

---
+++ Objects/system_armcm0plus.d
@@ -0,0 +1,10 @@
+./objects/system_armcm0plus.o: RTE\Device\ARMCM0P\system_ARMCM0plus.c \
+  C:\Users\Derviş\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\Device\ARM\ARMCM0plus\Include\ARMCM0plus.h \
+  C:\Users\Derviş\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm0plus.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\Derviş\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\Derviş\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\Derviş\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Users\Derviş\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\Device\ARM\ARMCM0plus\Include\system_ARMCM0plus.h
